1234fd44c5cbc04f1f086d7557bdb4fe615ec96f
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-04 20:53:31 -0300

    Livro de notas e teste
    Classe livro de notas possui um método. Classe de teste o executa.

---
+++ LivroDeNotas.java
@@ -0,0 +1,10 @@
+//alta coesão
+//você deseja que as suas classes sejam altamente coesas
+//uma classe altamente coesa é aquela que tem somente um propósito, somente uma finalidade, somente uma razão de ser, ela resolve somente um problema
+//alta coesão promove a reusabilidade e a manutenabilidade do seu código
+public class LivroDeNotas{
+    //método (comportamento)
+    public void exibirMensagem (){
+        System.out.println("Bem vindo ao livro de notas");
+    }
+}
---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,10 @@
+public class TesteLivroDeNotas {
+    public static void main(String[] args) {
+        //variável de referência
+        LivroDeNotas livroDeNotas;
+        //operador de construção de objetos: new
+        livroDeNotas = new LivroDeNotas();
+        //operador de acesso a membro: .
+        livroDeNotas.exibirMensagem();
+    }
+}
