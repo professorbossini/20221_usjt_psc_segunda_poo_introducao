8daf5aa7af0ff119b54c6efdc741f429172a67a1
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-04 21:25:54 -0300

    Versão em que nomeDaUC é variável de instância

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -1,13 +1,8 @@
-//alta coesão
-//você deseja que as suas classes sejam altamente coesas
-//uma classe altamente coesa é aquela que tem somente um propósito, somente uma finalidade, somente uma razão de ser, ela resolve somente um problema
-//alta coesão promove a reusabilidade e a manutenabilidade do seu código
 public class LivroDeNotas{
-    //método (comportamento)
-    //() é a lista de parâmetros desse método
 
-    //definição do método: aqui nós dissemos que ele existe
-    public void exibirMensagem(String uc){
-        System.out.println("Bem vindo ao livro de notas da UC " + uc);
+    String nomeDaUC;
+
+    public void exibirMensagem(){
+        System.out.println("Bem vindo ao livro de notas da UC " + nomeDaUC);
     }
 }
--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -3,13 +3,9 @@ import javax.swing.JOptionPane;
 public class TesteLivroDeNotas {
     public static void main(String[] args) {
         String nomeDaUC = JOptionPane.showInputDialog("Professor, qual o nome da UC?");
-        //variável de referência
         LivroDeNotas livroDeNotas;
-        //operador de construção de objetos: new
         livroDeNotas = new LivroDeNotas();
-        //operador de acesso a membro: .
-        //é a chamada do método
-        //chamar (call) um método significa colocá-lo em execução
-        livroDeNotas.exibirMensagem(nomeDaUC);
+        livroDeNotas.nomeDaUC = nomeDaUC;
+        livroDeNotas.exibirMensagem();
     }
 }
