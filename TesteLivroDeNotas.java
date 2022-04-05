6c0d2a107cf99ebcf0464f9a878568feec25d691
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-04 21:49:17 -0300

    Exemplo com duas instâncias e duas variáveis de referência

--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -3,9 +3,16 @@ import javax.swing.JOptionPane;
 public class TesteLivroDeNotas {
     public static void main(String[] args) {
         String nomeDaUC = JOptionPane.showInputDialog("Professor, qual o nome da UC?");
+        
         LivroDeNotas livroDeNotas;
         livroDeNotas = new LivroDeNotas();
         livroDeNotas.nomeDaUC = nomeDaUC;
         livroDeNotas.exibirMensagem();
+
+        LivroDeNotas livro2 = new LivroDeNotas();
+        //syntax sugar
+        //livro2.nomeDaUC = new String("Matemática");
+        livro2.nomeDaUC = "Matemática";
+        livro2.exibirMensagem();
     }
 }
